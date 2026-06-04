f4637faee3fe1bdff118e150ad153608500b22a8
ngocdat48gtvt-dev <ngocdat48gtvt@gmail.com>
2026-06-04 20:35:00 +0700

    cap nhật

--- build-dieu-hanh.bat
+++ build-dieu-hanh.bat
@@ -4,8 +4,7 @@ setlocal
 
 set "WEBSITE_DIR=%~dp0"
 set "APP_DIR=%WEBSITE_DIR%..\dieu-hanh-web"
-set "OUT_DH=%WEBSITE_DIR%dieu-hanh"
-set "OUT_DB=%WEBSITE_DIR%dashboard"
+set "DIST=%APP_DIR%\dist"
 
 if not exist "%APP_DIR%\package.json" (
   echo [LOI] Khong tim thay dieu-hanh-web
@@ -21,19 +20,27 @@ call npm run build
 if errorlevel 1 goto :fail
 popd
 
-for %%D in ("%OUT_DH%" "%OUT_DB%") do (
-  if exist "%%~fD" rmdir /s /q "%%~fD"
-  mkdir "%%~fD"
-  xcopy /e /i /y "%APP_DIR%\dist\*" "%%~fD\" >nul
-)
+call :deploy_folder "%WEBSITE_DIR%dashboard" "/dashboard"
+call :deploy_folder "%WEBSITE_DIR%dieu-hanh" "/dieu-hanh"
 
 echo.
-echo Da copy vao:
-echo   %OUT_DH%
-echo   %OUT_DB%
-echo.
-echo Commit ca hai thu muc + push len Vercel.
+echo Da copy vao dashboard va dieu-hanh (duong dan tuyet doi /assets).
+echo Commit + push repo website len Vercel.
 pause
+exit /b 0
+
+:deploy_folder
+set "TARGET=%~1"
+set "BASE=%~2"
+if exist "%TARGET%" rmdir /s /q "%TARGET%"
+mkdir "%TARGET%"
+xcopy /e /i /y "%DIST%\*" "%TARGET%\" >nul
+powershell -NoProfile -Command ^
+  "$p='%TARGET%\index.html'; $b='%BASE%'; $h=[IO.File]::ReadAllText($p); " ^
+  "$h=$h.Replace('./firebase-config.js',$b+'/firebase-config.js'); " ^
+  "$h=$h.Replace('./assets/',$b+'/assets/'); " ^
+  "if($h -notmatch '<base href'){ $h=$h.Replace('<head>','<head>`r`n    <base href=\"'+$b+'/\" />') }; " ^
+  "[IO.File]::WriteAllText($p,$h)"
 exit /b 0
 
 :fail
