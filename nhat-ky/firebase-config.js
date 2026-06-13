7bbb4e13e8e22be07c9eac15d481b9e048c2beb8
ngocdat48gtvt-dev <ngocdat48gtvt@gmail.com>
2026-06-13 21:27:37 +0700

    cập nhật

---
+++ nhat-ky/firebase-config.js
@@ -0,0 +1,9 @@
+/** Cùng Firebase project với app Android & web điều hành */
+window.__NHAT_KY_FIREBASE__ = {
+  apiKey: "AIzaSyBgLClpTtYEc-PwZSCfKFWc8HkoJbFPbj8",
+  authDomain: "quanlysuco-6797e.firebaseapp.com",
+  projectId: "quanlysuco-6797e",
+  storageBucket: "quanlysuco-6797e.firebasestorage.app",
+  messagingSenderId: "958751424697",
+  appId: "1:958751424697:web:landing-quanlysuco"
+};
